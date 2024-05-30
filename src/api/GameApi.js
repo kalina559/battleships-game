b8a2ea5870b380eb07d452777e315d211434b93b
Jakub Kalinowski <kalina559@gmail.com>
2024-05-30 12:37:10 +0200

    Introduced env/prod environments, connecting to API

--- src/api/GameApi.js
+++ src/api/GameApi.js
@@ -41,12 +41,64 @@
 // };
 
 
+
+
+
+
 // src/GameApi.js
 
-const previousShots = [];
+ const previousShots = [];
 
-const api = {
+// const api = {
+//   getOpponentShips: async () => {
+//     // Mocked opponent ships
+//     return [
+//       { size: 5, coordinates: [{ x: 0, y: 0 }, { x: 0, y: 1 }, { x: 0, y: 2 }, { x: 0, y: 3 }, { x: 0, y: 4 }] },
+//       { size: 4, coordinates: [{ x: 2, y: 2 }, { x: 2, y: 3 }, { x: 2, y: 4 }, { x: 2, y: 5 }] },
+//       { size: 3, coordinates: [{ x: 5, y: 5 }, { x: 5, y: 6 }, { x: 5, y: 7 }] },
+//       { size: 2, coordinates: [{ x: 7, y: 8 }, { x: 7, y: 9 }] },
+//       { size: 1, coordinates: [{ x: 9, y: 0 }] }
+//     ];
+//   },
+//   setUserShips: async (ships) => {
+//     // Mock set user ships
+//     console.log('User ships set:', ships);
+//     return;
+//   },
+//   getAIMove: async () => {
+//     let move;
+//     do {
+//       move = {
+//         x: Math.floor(Math.random() * 10),
+//         y: Math.floor(Math.random() * 10)
+//       };
+//     } while (previousShots.some(shot => shot.x === move.x && shot.y === move.y));
+//     previousShots.push(move);
+//     return move;
+//   }
+// };
+
+// export default api;
+
+
+
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: process.env.VUE_APP_API_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+export default {
   getOpponentShips: async () => {
+
+    var test = apiClient.get('/WeatherForecast');
+
+    console.log("api test")
+    console.log(test)
+    
     // Mocked opponent ships
     return [
       { size: 5, coordinates: [{ x: 0, y: 0 }, { x: 0, y: 1 }, { x: 0, y: 2 }, { x: 0, y: 3 }, { x: 0, y: 4 }] },
@@ -73,5 +125,3 @@ const api = {
     return move;
   }
 };
-
-export default api;
