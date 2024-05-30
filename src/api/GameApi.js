ef65afa8ba92d67cdfc173567f0db6effc1ec92b
Jakub Kalinowski <kalina559@gmail.com>
2024-05-30 14:21:48 +0200

    Using API to place opponent's ships

--- src/api/GameApi.js
+++ src/api/GameApi.js
@@ -94,19 +94,10 @@ const apiClient = axios.create({
 export default {
   getOpponentShips: async () => {
 
-    var test = apiClient.get('/WeatherForecast');
-
-    console.log("api test")
-    console.log(test)
-    
-    // Mocked opponent ships
-    return [
-      { size: 5, coordinates: [{ x: 0, y: 0 }, { x: 0, y: 1 }, { x: 0, y: 2 }, { x: 0, y: 3 }, { x: 0, y: 4 }] },
-      { size: 4, coordinates: [{ x: 2, y: 2 }, { x: 2, y: 3 }, { x: 2, y: 4 }, { x: 2, y: 5 }] },
-      { size: 3, coordinates: [{ x: 5, y: 5 }, { x: 5, y: 6 }, { x: 5, y: 7 }] },
-      { size: 2, coordinates: [{ x: 7, y: 8 }, { x: 7, y: 9 }] },
-      { size: 1, coordinates: [{ x: 9, y: 0 }] }
-    ];
+    const response = await apiClient.get('/ShipLocations/opponent');
+    console.log("ship locations")
+    console.log(response.data)
+    return response.data;
   },
   setUserShips: async (ships) => {
     // Mock set user ships
